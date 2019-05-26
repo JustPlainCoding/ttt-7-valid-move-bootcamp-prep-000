c04bbe7ada406ce5487eb666bc782a309cbe91aa
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-26 20:33:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -15,7 +15,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def position_taken?(position)
-  if position == "" || " " || nil
+  if position == "" ||position == " " || position == nil
     false
   else
     true
