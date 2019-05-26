8615d9da40776425d0728ace9c9ad9c4461ea7b4
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-26 20:27:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,13 @@
 # code your #valid_move? method here
 
-def valid_move?
+def valid_move?(board, position)
+  if position_taken(board[position])?
+    false
+  elsif
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
-def position_taken? 
+def position_taken?
+  
 end
